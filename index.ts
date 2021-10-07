f3259bd97d3c165f44d4e0edf2ff3f1fdbb90931
Maksym Shamko <max.shamko25@gmail.com>
2021-10-07 13:24:35 +0300

    working

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 import * as constants from './src/const';
 import { Helper as Math } from './src/math';
 import { TimeAgo as Time } from './src/timaAgo';
-import * as Cross from './src/cross';
-import * as DB from './src/db';
-import * as File from './src/file';
+import { Cross } from './src/cross';
+import { DB } from './src/db';
+import { getFileLinesSync } from './src/file';
 
 export { constants };
 export { Math };
 export { Time };
 export { Cross };
 export { DB };
-export { File };
+export { getFileLinesSync };
 
 export default {
   constants,
@@ -18,5 +18,5 @@ export default {
   Time,
   Cross,
   DB,
-  File,
+  getFileLinesSync,
 };
